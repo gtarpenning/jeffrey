198ffdf70f522e6c32a067f0a866af80199d1066
gtarpenning <griffin.tarpenning@wandb.com>
2023-10-15 15:39:09 -0700

    simpler

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,37 +23,21 @@ if file:
         with st.expander(label="Report", expanded=True):
             bot = OpenAIWrapper(model_name=ChatModel.GPT_35_TURBO_16K_PINNED, disable_wandb=True)
             file_contents = read_docx(file)
-            header_form, intake_form = file_contents.split("—————")
-            if test:
-                intake_form = intake_form[:1000]
 
             markdown_str = "# Psychiatric History Report\n"
-
-            # first make the section header:
-            header_messages=[
-                {"role": "system", "content": jeff.header_prompt},
-                {"role": "user", "content": header_form}
-            ]
-
             stream_text = st.empty()
-            for chunk in bot.call_chatgpt_stream(messages=header_messages):
-                msg = chunk["choices"][0]["delta"].get("content")
-                if msg:
-                    markdown_str += msg.replace("$", "\$")
-                stream_text.write(markdown_str)
-
-            markdown_str += "\n"
             # then make the report body:
             question_messages=[
-                {"role": "system", "content": f"Patient demographic info: {markdown_str}"},
                 {"role": "system", "content": jeff.questionare_prompt},
-                {"role": "user", "content": intake_form}
+                {"role": "user", "content": file_contents}
             ]
             for chunk in bot.call_chatgpt_stream(messages=question_messages):
                 msg = chunk["choices"][0]["delta"].get("content")
                 if msg:
                     markdown_str += msg.replace("$", "\$")
                 stream_text.write(markdown_str)
+
+            markdown_str += "\n" + jeff.JEFFREY_SIGNATURE
 
         st.write('Processing complete!')
         os.makedirs('./output/md', exist_ok=True)
